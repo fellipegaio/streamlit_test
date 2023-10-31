aa6d23d1051328f97efc7731597fa228464e6b2d
Fellipe Gaio <ofellipegaio@gmail.com>
2023-10-31 23:35:19 +0000

    final

--- app.py
+++ app.py
@@ -6,8 +6,8 @@ car_data = pd.read_csv('vehicles.csv') # lendo os dados
 
 st.header('Visualização interativa de dados sobre anúncios de veículos') # cabeçalho explicativo
 
-hist_button = st.button('Criar histograma') # criar um botão para o histograma
-scatter_button = st.button('Criar gráfico de dispersão') # criar um botão para o gráfico de dispersão
+hist_button = st.button('Ver somente o histograma') # criar um botão para o histograma
+scatter_button = st.button('Ver somente o gráfico de dispersão') # criar um botão para o gráfico de dispersão
 
 if hist_button: # se o botão de histograma for clicado
 # escrever uma mensagem
@@ -25,9 +25,10 @@ if scatter_button: # se o botão de gráfico de dispersão for clicado
     # exibir um gráfico Plotly interativo
     st.plotly_chart(fig_scatter, use_container_width=True)
 
+st.write('Para ver mais de um gráfico simultaneamente, use as caixas de seleção abaixo:')
 # criar caixas de seleção
-build_histogram = st.checkbox('Gerar um histograma dos anúncios pela condição dos veículos')
-build_scatter = st.checkbox('Gerar um gráfico de dispersão sobre anúncios cruzando o ano de fabricação do veículo com o seu preço de venda')
+build_histogram = st.checkbox('Gerar histograma dos anúncios pela condição dos veículos')
+build_scatter = st.checkbox('Gerar gráfico de dispersão sobre anúncios cruzando o ano de fabricação do veículo com o seu preço de venda')
 
 if build_histogram: # se o histograma for selecionado
 # escrever a mensagem
