bcc49fe46d42e25fb1af19e83a1e31140dbd168e
Fellipe Gaio <ofellipegaio@gmail.com>
2023-10-31 21:00:06 +0000

    rotulando os graficos

--- app.py
+++ app.py
@@ -3,14 +3,15 @@ import plotly.express as px
 import streamlit as st
      
 car_data = pd.read_csv('vehicles.csv') # lendo os dados
-hist_button = st.button('Criar histograma') # criar um botão
-scatter_button = st.button('Criar gráfico de dispersão') # criar um botão
 
 st.header('Visualização interativa de dados sobre veículos')
 
+hist_button = st.button('Criar histograma') # criar um botão
+scatter_button = st.button('Criar gráfico de dispersão') # criar um botão
+
 if hist_button: # se o botão for clicado
 # escrever uma mensagem
-    st.write('Criando um histograma para o conjunto de dados de anúncios de vendas de carros')
+    st.write('Histograma de anúncios de vendas de carros de acordo com a condição do veículo')
     # criar um histograma
     fig_hist = px.histogram(car_data, x="condition")
     # exibir um gráfico Plotly interativo
@@ -18,9 +19,9 @@ if hist_button: # se o botão for clicado
 
 if scatter_button: # se o botão for clicado
 # escrever uma mensagem
-    st.write('Criando um gráfico de dispersão para o conjunto de dados de anúncios de vendas de carros')
+    st.write('Gráfico de dispersão sobre anúncios de vendas de carros: ano de fabricação v preço')
     # criar um gráfico de dispersão
-    fig_scatter = px.scatter(car_data, x="model_year", y="price")
+    fig_scatter = px.scatter(car_data, x="model_year", x_label="Ano de fabricação" y="price", y_label="Preço")
     # exibir um gráfico Plotly interativo
     st.plotly_chart(fig_scatter, use_container_width=True)
 
