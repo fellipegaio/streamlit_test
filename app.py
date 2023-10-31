fec276e895cb17bd9d3a3bdbad2261f846bc2731
Fellipe Gaio <ofellipegaio@gmail.com>
2023-10-31 23:15:38 +0000

    caixas de selecao

--- app.py
+++ app.py
@@ -26,7 +26,22 @@ if scatter_button: # se o botão for clicado
     st.plotly_chart(fig_scatter, use_container_width=True)
 
 # criar uma caixa de seleção
-#build_histogram = st.checkbox('Criar um histograma')
+build_histogram = st.checkbox('Criar um histograma')
 
-#if build_histogram: # se a caixa de seleção for selecionada
-#    st.write('Criando um histograma para a coluna odometer')
+if build_histogram: # se a caixa de seleção for selecionada
+# escrever uma mensagem
+    st.write('Histograma de anúncios de vendas de carros de acordo com a condição do veículo')
+    # criar um histograma
+    fig_hist = px.histogram(car_data, x="condition")
+    # exibir um gráfico Plotly interativo
+    st.plotly_chart(fig_hist, use_container_width=True)
+
+build_scatter = st.checkbox('Criar um histograma')
+
+if build_scatter: # se a caixa de seleção for selecionada
+# escrever uma mensagem
+    st.write('Gráfico de dispersão sobre anúncios de vendas de carros: ano de fabricação v preço')
+    # criar um gráfico de dispersão
+    fig_scatter = px.scatter(car_data, x="model_year", y="price")
+    # exibir um gráfico Plotly interativo
+    st.plotly_chart(fig_scatter, use_container_width=True)
