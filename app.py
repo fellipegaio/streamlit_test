5aa26df77ce1335a6d5902861ed0d20f830d4427
Fellipe Gaio <ofellipegaio@gmail.com>
2023-10-31 20:12:27 +0000

    codigo organizado

--- app.py
+++ app.py
@@ -6,24 +6,26 @@ car_data = pd.read_csv('vehicles.csv') # lendo os dados
 hist_button = st.button('Criar histograma') # criar um botão
 scatter_button = st.button('Criar gráfico de dispersão') # criar um botão
 
-st.header('Header')
+st.header('Visualização interativa de dados sobre veículos')
 
 if hist_button: # se o botão for clicado
 # escrever uma mensagem
     st.write('Criando um histograma para o conjunto de dados de anúncios de vendas de carros')
+    # criar um histograma
+    fig_hist = px.histogram(car_data, x="odometer")
+    # exibir um gráfico Plotly interativo
+    st.plotly_chart(fig_hist, use_container_width=True)
 
 if scatter_button: # se o botão for clicado
 # escrever uma mensagem
     st.write('Criando um gráfico de dispersão para o conjunto de dados de anúncios de vendas de carros')
-
-# criar um histograma
-fig = px.histogram(car_data, x="odometer")
-
-# exibir um gráfico Plotly interativo
-st.plotly_chart(fig, use_container_width=True)
+    # criar um gráfico de dispersão
+    fig_scatter = px.scatter(car_data, x="odometer")
+    # exibir um gráfico Plotly interativo
+    st.plotly_chart(fig_scatter, use_container_width=True)
 
 # criar uma caixa de seleção
-build_histogram = st.checkbox('Criar um histograma')
+#build_histogram = st.checkbox('Criar um histograma')
 
-if build_histogram: # se a caixa de seleção for selecionada
-    st.write('Criando um histograma para a coluna odometer')
+#if build_histogram: # se a caixa de seleção for selecionada
+#    st.write('Criando um histograma para a coluna odometer')
