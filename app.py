a719db7c268b193f1830cd377a28ec62da5058aa
Fellipe Gaio <ofellipegaio@gmail.com>
2024-01-07 17:24:57 +0000

    Translated

--- app.py
+++ app.py
@@ -2,57 +2,57 @@ import pandas as pd
 import plotly.express as px
 import streamlit as st
      
-car_data = pd.read_csv('vehicles.csv') # lendo os dados
+car_data = pd.read_csv('vehicles.csv') # reading the data
 
-st.header('Visualização interativa de dados sobre anúncios de veículos') # cabeçalho explicativo
-st.write('Para ver mais de um gráfico simultaneamente, use as caixas de seleção abaixo.')
-st.write('Para ver o histograma de condição dos veículos anunciados, use o botão ao final.')
+st.header('Interactive Data Visualization of Vehicle Advertisements') # explanatory jheader
+st.write('To view more than one chart simultaneously, use the checkboxes below.')
+st.write("To view the histogram of the advertised vehicles' condition, use the button at the bottom.")
 
 # criar caixas de seleção
-hist_price = st.checkbox('Histograma de Preço') # criar uma caixa de seleção para o histograma de preço
-hist_year = st.checkbox('Histograma de Ano de Fabricação') # criar uma caixa de seleção para o histograma de ano
-scatter_button = st.checkbox('Gráfico de dispersão entre Ano de Fabricação vs Preço do Veículo') # criar uma caixa de seleção para o gráfico de dispersão
-pie_fuel = st.checkbox('Gráfico de pizza de Tipo de Combustível') # criar uma caixa de seleção para o gráfico de pizza
+hist_price = st.checkbox('Price Histogram') # creating a checkbox for the price histogram
+hist_year = st.checkbox('Manufacturing Year Histogram') # creating a checkbox for the year histogram
+scatter_button = st.checkbox('Scatter Plot between Manufacturing Year and Price') # creating a checkbox for the scatter plot
+pie_fuel = st.checkbox('Fuel Type Pie Chart') # creating a checkbox for the pie chart
 
-if hist_price: # se o histograma de preço for clicado
-# escrever uma mensagem
-    st.write('Histograma dos preços dos veículos nos anúncios')
-    # criar um histograma
+if hist_price: # if the price histogram is clicked
+# write the message
+    st.write('Histogram of vehicle prices in the advertisements')
+    # plot the histogram
     fig_price = px.histogram(car_data, x="price")
-    # exibir um gráfico Plotly interativo
+    # showing an interactive Plotly chart
     st.plotly_chart(fig_price, use_container_width=True)
 
-if hist_year: # se o histograma por ano for clicado
-# escrever uma mensagem
-    st.write('Histograma do ano de fabricação dos veículos nos anúncios')
-    # criar um histograma
+if hist_year: # if the manufacturing year of vehicles histogram is clicked
+# write the message
+    st.write('Histogram of the manufacturing year of vehicles in the advertisements')
+    # plot the histogram
     fig_year = px.histogram(car_data, x="model_year")
-    # exibir um gráfico Plotly interativo
+    # showing an interactive Plotly chart
     st.plotly_chart(fig_year, use_container_width=True)
 
-if scatter_button: # se o gráfico de dispersão for clicado
-# escrever uma mensagem
-    st.write('Gráfico de dispersão sobre anúncios de vendas de carros: Ano de Fabricação versus Preço do Veículo')
-    # criar um gráfico de dispersão
+if scatter_button: # if the scatter plot button is selected
+# write the message
+    st.write('Scatter plot on car sales advertisements: Manufacturing Year versus Vehicle Price')
+    # plot the scatter plot
     fig_year_price = px.scatter(car_data, x="model_year", y="price")
-    # exibir um gráfico Plotly interativo
+    # showing an interactive Plotly chart
     st.plotly_chart(fig_year_price, use_container_width=True)
 
-if pie_fuel: # se o gráfico de pizza for clicado
-# escrever uma mensagem
-    st.write('Gráfico de pizza dos tipos de combustível nos anúncios')
-    # criar um gráfico de pizza
+if pie_fuel: # if the pie chart is selected
+# write the message
+    st.write('Pie chart of fuel types in the advertisements')
+    # plot the pie chart
     fig_fuel = px.pie(car_data, names='fuel')
-    # exibir um gráfico Plotly interativo
+    # showing an interactive Plotly chart
     st.plotly_chart(fig_fuel, use_container_width=True)
 
-# Criar botão de histograma
-build_histogram = st.button('Histograma da condição dos veículos anunciados')
+# creating the histogram button
+build_histogram = st.button('Vehicle Condition Histogram')
 
-if build_histogram: # se o histograma for selecionado
-# escrever a mensagem
-    st.write('Histograma de anúncios de vendas de carros de acordo com a condição do veículo')
-    # criar um histograma
+if build_histogram: # if he histogram button is clicked
+# write the message
+    st.write("Histogram of car sales advertisements based on the vehicle's condition")
+    # create the histogram
     fig_hist = px.histogram(car_data, x="condition")
-    # exibir um gráfico Plotly interativo
+    # showing an interactive Plotly chart
     st.plotly_chart(fig_hist, use_container_width=True)
